1c559530f6ecc62c8d48681f51fccb6c753a3b59
AFVA <avillarraga@opc.com.co>
2024-03-01 15:28:06 -0500

    Se crea metodo para validar el user frente la db usando UseGuards y AuthGuard de @nestjs/passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { AuthService } from './service/auth.service'
 
-@Module({})
+import { LocalStrategy } from './strategies/local.strategy'
+import { UsersModule  } from '../users/users.module'
+import { AuthController } from './controllers/auth.controller';
+
+@Module({
+  imports: [PassportModule,UsersModule],
+  providers: [AuthService,LocalStrategy],
+  controllers: [AuthController]
+})
 export class AuthModule {}
